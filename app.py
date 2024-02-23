2582ad6b8ea512e67ed251774827dd6021616dd6
Diego Faria <diegoafbrito12@gmail.com>
2024-02-23 15:41:02 -0400

    initialization

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import jsonify
+from flask import request, render_template
+
+app = Flask(__name__)
+
+@app.route("/chatbot", methods=["GET"])
+def chatbot():
+    import chatbot
+
+    message = request.json['message']
+    res = chatbot.get_message(message)
+    
+    return jsonify({'res': res})
+
+@app.route('/')
+def chatbot_ui():
+    return render_template(r'index.html')
